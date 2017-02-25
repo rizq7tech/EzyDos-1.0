84cbbaa78dce78ae666424611033ed8c0e6a0fb5
Macbook <Macbook@macbooks-macbook-pro.local>
2017-02-25 14:00:42 +0800

    Initial Commit

---
+++ EzyMekah/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EzyMekah
+//
+//  Created by Macbook on 2/25/17.
+//  Copyright © 2017 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
