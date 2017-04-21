1507d1695b353ff3a81b693163cb51454ce614e0
IntelLigenEthics Hybrid Tech <7appsinc@gmail.com>
2017-04-21 21:49:13 +0800

    Update services.js

--- EzyDos/www/js/services.js
+++ EzyDos/www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('your_app_name.services', [])1
+angular.module('your_app_name.services', [])
 
 .service('FeedList', function ($rootScope, FeedLoader, $q){
 	this.get = function(feedSourceUrl) {
